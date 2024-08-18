470b902d57b9e14062c91bb616cd00ff0e2d6cf8
adam piziak <adpiziak@gmail.com>
2024-08-18 12:43:36 -0400

    github pages?

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@solidjs/start/config";
 export default defineConfig({
   server: {
     static: true,
-    preset: "cloudflare-pages-static",
+    preset: "github-pages",
     prerender: {
       crawlLinks: true,
     },
